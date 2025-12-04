075a45e5e1c7f6e94b500b7703c30d80b63c085b
Absolutely Anonymous <nobody@nomailtomeplease.com>
2025-12-04 18:12:40 +0000

    Fix non-lfs files. Add umask to get.sh

--- get.sh
+++ get.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c39bdeed236d615c936ebb9286eae5d0f2b67b564e16e266fad0ae4f43a74bcd
-size 7484
+oid sha256:938a01ec73b83bf976ad0904e214caf5096a84fcee2b0dece40b2e6a1ebaca8c
+size 7538
